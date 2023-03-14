bb8baa6fd75afb681174e5633b00fb6fc66cde81
Alex Njagi <1992anjagi@gmail.com>
2023-03-14 21:16:56 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -14,12 +14,14 @@ function distanceTravelledInFeet(start, destination) {
 
 function calculatesFarePrice(start, destination) {
   //returns the fare for the customer
-  if (distanceTravelledInFeet(Math.abs(start-destination)*264) <=400){
+  const distance = distanceTravelledInFeet(start, destination);
+  
+  if (distance<=400){
     return 0;
-  } else if (distanceTravelledInFeet(Math.abs(start-destination)*264) <=2000){
-    return (Math.abs(start - destination) * 264)-(400)*(0.02);
-  } else if (distanceTravelledInFeet(Math.abs(start-destination)*264) <=2500){
-    return (Math.abs(start - destination) * 264 *25);
+  } else if (distance > 400 && distance <=2000){
+    return (distance-400)*.02;
+  } else if (distance >2000 && distance <=2500){
+    return 25;
   } else return 'cannot travel that far';
 }
 
